0eb0a5942d5b1e21457f5b740de5db4e2797d7e1
Simon Hudson <simon.hudson@experian.com>
2015-12-10 07:43:43 +0000

    1) Add KO 2) Initial Ajax call

--- js/main.js
+++ js/main.js
@@ -8,6 +8,20 @@ var Petition = {
             url: 'https://petition.parliament.uk/petitions/' + id + '.json',
             method: 'get'
         });
+    },
+
+    createModel: function(data) {
+        return new Petition.Model(data);
+    },
+
+    Model: function(data) {
+        var attributes = data.data.attributes;
+        var self = attributes;
+        console.log(self);
     }
     
 };
+
+Petition.get('114003').then(function(data) {
+    Petition.createModel(data);
+});
