6da2d433f6c1eddb48bfeb727421c22dc2e12f3e
Simon Hudson <simon.hudson@experian.com>
2016-01-18 11:37:06 +0000

    Add footer links

--- includes/postcontent.inc.php
+++ includes/postcontent.inc.php
@@ -1,5 +1,14 @@
 </div><!--/wrap-->
 
+<footer>
+    <div class="wrap">
+        <small>
+            Built by <a href="http://www.heyhudson.com" target="_blank">Simon Hudson<span class="hidden"> (opens in a new window)</span></a><br />
+            Data from <a href="https://petition.parliament.uk" target="_blank">petition.parliament.uk<span class="hidden"> (opens in a new window)</span></a>
+        </small>
+    </div>
+</footer>
+
 <!--libraries-->
 <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="js/url.js"></script>
