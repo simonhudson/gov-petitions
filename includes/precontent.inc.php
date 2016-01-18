573b429048b372ecf8c1a36c44fe227b4f7b37cd
Simon Hudson <simon.hudson@experian.com>
2016-01-18 11:30:21 +0000

    Remove font awesome link

--- includes/precontent.inc.php
+++ includes/precontent.inc.php
@@ -4,7 +4,6 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>Parliament Petitions</title>
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 <script src="http://ajax.aspnetcdn.com/ajax/knockout/knockout-3.3.0.js"></script>
 <link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
